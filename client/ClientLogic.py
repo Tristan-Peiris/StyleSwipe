286208bf1f9287eaf404206c69881ce3809524ec
loh0021 <loh0021@gwsc.vic.edu.au>
2024-08-27 12:00:09 +1000

    Update ClientLogic.py

--- client/ClientLogic.py
+++ client/ClientLogic.py
@@ -1,53 +1,70 @@
-import tkinter as tk
-import csv
-import random
 from os import listdir
-previousimage = None
-randomimage = 1
-# Input: None
-# Proccess: Get a random image
-# Returns the image
-def displayImage(window: tk.Tk, frame: tk.Frame, moreInfoFrame: tk.Frame, display) -> tk.Frame:
-    global randomimage, previousimage
-    totalimages = len(listdir("images/styles"))
-    randomimage = random.randint(1,totalimages)
-    while randomimage == previousimage:
-        randomimage = random.randint(1, totalimages)
+import tkinter as tk
+import random
+import csv
 
-    previousimage = randomimage
+previous = None # The previous image
+imageName = None # The name of the image
+
+# Input: Window
+#        The frame the stuff is in
+#        Display object
+#        The frame to display more information
+#        The type of motion
+# Process: Gets the next image to display
+#          Places the image in the window
+#          Destroys the previous image
+# Output: None
+# Displays the next image
+def nextImage(window: tk.Tk, frame: tk.Frame, display, moreInfoFrame: tk.Frame, type: str = None):
+    global previous # Getting what the user can see
+    global imageName # Getting the name of the image
+
+    save_csv(imageName, type)
+
+    if display.moving == True: # Preventing action is stuff is already moving
+        return
+
+    next = display.showFit(window, frame, moreInfoFrame, display) # Getting the next image
+    if previous != None: # Playing in the next image
+        next.lower(previous)
+    next.place(x=162.5,y=240,anchor="center")
+    if previous != None:
+        if type == "like": # Moving the image
+            display.slideRight(window, previous, 325, 162.5, "center", 1)
+        elif type == "dislike":
+            display.slideLeft(window, previous, 325, 162.5, "center", 1)
+        else:
+            raise ValueError("Invalid type") # Error handling
+        previous.destroy() # Destroying the previous image
+    previous = next # Setting the previous image to the current image
 
 
-    
-    currentimage = display.getDisplayImage(frame, display.imageStorage, f"images/styles/fitpic{randomimage}.png", (278, 402))
-    
-    infoBar = display.getButtonImage(currentimage, display.imageStorage, "images/bottombar3.png", (290, 35), command=lambda: display.moveUp(window, moreInfoFrame, 325, 650, "n", 2))
-    
-    likes = tk.Label(currentimage, text="10K", fg="white", font=("Helvetica bold", 7), bg="#ff6c5c")
-    
-    likes.place(x=261, y=395.5, anchor="center")
-    infoBar.place(x=135.5, y=405, anchor="s")
 
-    currentimage.lower()
-    currentimage.place(x=162.5,y=240,anchor="center")
-    print("done")
-    
-    return currentimage
-    
-def likeImage():
-    global currentimage
-    global randomimage
+def darkMode(value: bool, window: tk.Tk, currentframe: tk.Frame, navFrame: tk.Frame):
+    if value == True:
+      window.configure(bg="#212120")
+      currentframe.configure(bg="#212120")
+      navFrame.configure(bg="#212120")
+    else:
+      window.configure(bg="white")
+      currentframe.configure(bg="white")
+      navFrame.configure(bg="white")
+
+def save_csv(imageName: str, type: str):
     with open("closet.csv", "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([f"images/styles/fitpic{randomimage}.png"])
-
-
-def dislikeImage():
-    displayImage()
+        writer.writerow([imageName, type])
 
 def clear_csv():
     with open("closet.csv", "w", newline="") as file:
         pass
 
+def selectPhoto(currentFrame: tk.Frame, display) -> tk.Frame:
+    global imageName
+    randomImage = random.randint(1, len(listdir('images/styles')))
+    imageName = f"fitpic{randomImage}.png"
+    return display.getDisplayImage(currentFrame, display.imageStorage, f"images/styles/fitpic{randomImage}.png", (278, 402))
 
 clear_csv()
 
